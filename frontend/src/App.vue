d0f9913c98f715ed00ac7a93fa09c040d6960395
meold <yura.yazupol.official@gmail.com>
2022-07-12 19:00:49 +0300

    bootstraped TronLink

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <h1>Test</h1>
 
-  <ipfs-info />
+  <hr>
+  <tron-link-helper />
+  <hr>
 
+  <ipfs-info />
 </template>
 
 <script>
 import IpfsInfo from './components/IpfsInfo.vue';
+import TronLinkHelper from './components/TronLinkHelper.vue';
 
 export default {
   components: {
-    IpfsInfo
+    IpfsInfo,
+    TronLinkHelper
   },
   props: [],
   data() {
