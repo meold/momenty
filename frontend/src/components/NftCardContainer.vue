d9692fb65175ff8097e2d54b77afc8b424729f6a
Roman Zhbadynskyi <zhbadynskyi@gmail.com>
2022-07-17 04:01:44 +0300

    minor buggs fixes

--- frontend/src/components/NftCardContainer.vue
+++ frontend/src/components/NftCardContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full overflow-hidden relative shadow-[0_8px_60px_rgba(0,0,0,0.05)] rounded-[16%/9%] before:block before:content-[''] before:w-full before:pt-[177%]">
+  <div class="w-full overflow-hidden isolate relative shadow-[0_8px_60px_rgba(0,0,0,0.05)] rounded-[16%/9%] before:block before:content-[''] before:w-full before:pt-[177%]">
     <div class="absolute inset-0">
       <slot />
     </div>
--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container inline-flex space-x-2.5 items-center justify-between py-10">
-    <p class="text-sm leading-none">© Ukrainian Magicals 2018-2022. All rights reserved</p>
+    <p class="text-sm leading-none">© 2018-{{ new Date().getFullYear() }} Ukrainian Magicals.</p>
     <div class="flex space-x-5 items-start justify-start">
       <a href="#" class="text-sm leading-none hover:underline hover:opacity-60">Terms of Use</a>
       <a href="#" class="text-sm leading-none hover:underline hover:opacity-60">Privacy Policy</a>
