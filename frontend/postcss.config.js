28bd2af07ad12070e4de88b78000f8ae1cf152b7
Roman Zhbadynskyi <zhbadynskyi@gmail.com>
2022-07-21 23:13:55 +0300

    bump

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: {
     'tailwindcss': {},
-    'postcss-preset-env': {},
-    'autoprefixer': {},
+    'postcss-preset-env': {
+      autoprefixer: {}
+    },
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
 };
