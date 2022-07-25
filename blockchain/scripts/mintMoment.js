8f53cd1f6b856bec803238f5509ae01a1d57b208
meold <yura.yazupol.official@gmail.com>
2022-07-25 23:38:36 +0300

    script

--- blockchain/scripts/mintMoment.js
+++ blockchain/scripts/mintMoment.js
@@ -2,15 +2,23 @@ async function main() {
   const MomentContract = await ethers.getContractFactory("MomentContract");
   const momentContract = await MomentContract.attach(process.env.MOMENT_CONTRACT);
 
-  const momentId = await momentContract.mint(
-    "MMNT2",
-    "A brief intro about the amazing moment.",
-    "https://main-nftmoments-incoming-media.s3.eu-central-1.amazonaws.com/d78a31bf-fefd-4d5c-8de8-1f4d648763e7.png",
-    "https://main-nftmoments-incoming-media.s3.eu-central-1.amazonaws.com/80c0f263-a162-4ec0-96ca-23cef32e5fad.mp4",
-    0
-  );
+  const res1 = await momentContract.momentMetadata(0);
+  console.log(res1);
 
-  console.log(momentId);
+  const MomentSaleContract = await ethers.getContractFactory("MomentSaleContract");
+  const momentSaleContract = await MomentSaleContract.attach("0xFE6946500Afb7C2f3B8B45D77AfbE4CE07541cEa");
+
+  const res2 = await momentSaleContract.momentMetadata(0);
+  console.log(res2);
+  // const momentId = await momentContract.mint(
+  //   "MMNT2",
+  //   "A brief intro about the amazing moment.",
+  //   "https://main-nftmoments-incoming-media.s3.eu-central-1.amazonaws.com/d78a31bf-fefd-4d5c-8de8-1f4d648763e7.png",
+  //   "https://main-nftmoments-incoming-media.s3.eu-central-1.amazonaws.com/80c0f263-a162-4ec0-96ca-23cef32e5fad.mp4",
+  //   0
+  // );
+
+  // console.log(momentId);
 }
 
 main()
